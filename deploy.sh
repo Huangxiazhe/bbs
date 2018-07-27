4c494cb9c1932d3ac3e688478653215d14afe149
Huangxiazhe <30306896+Huangxiazhe@users.noreply.github.com>
2018-07-27 13:38:43 +0800

    更新部署文本

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
-# 1. 拉代码到 /var/www/bbs
+# 1. 拉代码到 /var/www/bbs-server
 # 2. 执行 bash deploy.sh
 
 set -ex
-deploy_directory=/var/www/web21
+deploy_directory=/var/www/bbs-server
 
 # 系统设置
 apt-get -y install  zsh curl ufw
@@ -24,7 +24,7 @@ debconf-set-selections database_secret.conf
 debconf-set-selections postfix.conf
 
 apt-get install -y git supervisor nginx python3.6 mysql-server postfix
-python3.6 /var/www/web21/get-pip.py
+python3.6 /var/www/bbs-server/get-pip.py
 pip3 install jinja2 flask gevent gunicorn pymysql flask_sqlalchemy flask_mail flask_admin
 
 # 删掉 nginx default 设置
@@ -32,10 +32,10 @@ rm -f /etc/nginx/sites-enabled/default
 rm -f /etc/nginx/sites-available/default
 
 # 建立一个软连接
-ln -s -f /var/www/bbs/bbs.conf /etc/supervisor/conf.d/bbs.conf
+ln -s -f /var/www/bbs-server/bbs.conf /etc/supervisor/conf.d/bbs.conf
 # 不要再 sites-available 里面放任何东西
-ln -s -f /var/www/bbs/bbs.nginx /etc/nginx/sites-enabled/bbs
-chmod -R o+rwx /var/www/web21
+ln -s -f /var/www/bbs-server/bbs.nginx /etc/nginx/sites-enabled/bbs
+chmod -R o+rwx /var/www/bbs-server
 
 # 重启服务器
 service supervisor restart
